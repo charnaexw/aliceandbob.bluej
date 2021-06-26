be6d142e7e4af2d7685e53d16ebb4ef7479d5adb
charnaexw <charnaexw@gmail.com>
2021-06-26 00:00:43 -0400

    Changed somethings

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -4,31 +4,54 @@
  * @date 5/19/21 5:05 PM
  */
 public class AliceAndBobEngine {
+    
     /**
      * return `true` if the input value is "Alice"
      * @param input - this value is variable: it has the potential to be many things
      * @return `true` if `input` is "Alice"
      */
     public Boolean isAlice(String input) {
-        return null;
-    }
-
+              
+        if("Alice".equals(input))
+        {
+            return true;
+        }else
+        { return false; 
+        } 
+        
+        }
+        
+    
     /**
      * return `true` if the input value is "Bob"
      * @param input - this value is variable: it has the potential to be many things
      * @return `true` if `input` is "Bob"
      */
-    public Boolean isBob(String input) {
-        return null;
-    }
-
+    public Boolean isBob(String input){
+    
+        if("Bob".equals(input))
+        {
+            return true;
+        }else
+        { return false; 
+        } 
+        
+        }
+    
     /**
      * return `true` if the input value is "Alice" or "Bob"
      * @param input - this value is variable: it has the potential to be many things
      * @return `true` if `input` is "Alice" or "Bob"
      */
     public Boolean isAliceOrBob(String input) {
-        return null;
+        
+        if(input == "Alice" || input == "Bob"){ 
+            return true;
+        }
+        else{
+            return false;
+        }
+       
     }
 
     /**
@@ -42,6 +65,12 @@ public class AliceAndBobEngine {
      * @return respective String value
      */
     public String getGreeting(String input) {
-        return null;
-    }
+      if(input == "Alice" || input == "Bob"){
+          
+          return "Hello, " + input + "!";          
+      }else{
+      
+      return "Begone, " + input + "! You're a stranger!";
+}
+}
 }
